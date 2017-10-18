6e5696b205f4acb54527a32f3680366dad1ad33c
Daniela Flores <kenzie85@hotmail.com>
2017-10-18 09:49:07 -0500

    Java files

---
+++ Java/Básicos/Prueba1/src/prueba1/Prueba1.java
@@ -0,0 +1,47 @@
+
+package prueba1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dflores
+ */
+public class Prueba1 {
+    // Atributos
+    protected int edad;
+    protected String nombre;
+    
+    // Constructor
+    public Prueba1(int edad, String nombre)
+    {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+    
+    // Funciones
+    public int getEdad()
+    {
+        return edad;
+    }
+    
+    protected void printName()
+    {
+     System.out.println("Mi nombre es: " + nombre);
+    }
+    
+    
+    // Main
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);    
+        System.out.println("Escribe tu nombre: ");
+        String nombre = s.nextLine();
+        System.out.println("Escribe tu edad: ");
+        int edad = s.nextInt();
+        Prueba1 ejemplo = new Prueba1(20, nombre);
+        ejemplo.printName();
+        System.out.println();
+        System.out.println("Mi edad es: " + ejemplo.getEdad());
+    }
+    
+}
---
+++ Java/Básicos/VentanaSimple.java
@@ -0,0 +1,44 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+/**
+ *
+ * @author dflores
+ */
+public class VentanaSimple extends JFrame{
+    public VentanaSimple()
+    {
+        setBounds(50,50,500,500);
+        
+        JLabel l = new JLabel("Escribe tu nombre: ");
+        JTextField nombre = new JTextField("                                    ");
+        JButton button = new JButton("Saludo");
+        JLabel saludo = new JLabel();
+        setLayout(new FlowLayout());
+        
+        add(l);
+        add(nombre);
+        add(button);
+        add(saludo);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = nombre.getText();
+                saludo.setText("Hola " + name);
+            }
+        });
+        
+        setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+       VentanaSimple v = new VentanaSimple();
+    }
+}
